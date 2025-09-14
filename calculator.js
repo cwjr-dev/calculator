c52f6af2287c4e3fe4275e51b6284c612aac5e67
CulbertW <jrdevpath87@gmail.com>
2025-09-14 15:59:14 -0500

    feat(buttons): add handleClick function
    Implement handleClick function to handle the operation
    for the type of button clicked

--- calculator.js
+++ calculator.js
@@ -1,5 +1,14 @@
 "use strict";
 
+const buttons = document.querySelector(".buttons");
+
+const operation = {
+    operand1: 0,
+    operator: "",
+    operand2: 0
+};
+
+// math operators
 function add(operand1, operand2) {
     return operand1 + operand2;
 }
@@ -20,3 +29,22 @@ function divide(operand1, operand2) {
         return "Can't divide by 0";
     }
 }
+
+// handles the type of button clicked (e.g. number, operator, delete, etc.)
+function handleClick(event) {
+    const element = event.target;
+
+    if (element.tagName === "BUTTON") {
+        const btnType = ["all-clear-btn", "clear-btn", "delete-left-btn", "operator-btn", "number-btn",
+            "plus-minus-btn", "decimal-btn", "equal-btn"
+        ].find(cls => element.classList.contains(cls));
+
+        switch(btnType) {
+     
+
+        }
+    }
+}
+
+
+buttons.addEventListener("click", handleClick);
